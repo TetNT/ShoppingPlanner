cf08f94819ece414a1ff3ceb7c147e0b3a8db1bf
kiril <kirillgrayson@gmail.com>
2022-02-01 14:55:24 +0200

    Code cleanups

--- app/src/main/java/com/tetsoft/planshopping/MainActivity.kt
+++ app/src/main/java/com/tetsoft/planshopping/MainActivity.kt
@@ -2,8 +2,6 @@ package com.tetsoft.planshopping
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.tetsoft.planshopping.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,9 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-//        val navHost =
-//            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-//        binding.mainBottomNavigation.setupWithNavController(navHost.navController)
     }
 }
