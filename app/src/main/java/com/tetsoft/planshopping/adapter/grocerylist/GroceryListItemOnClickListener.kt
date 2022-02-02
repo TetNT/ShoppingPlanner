4d1821fc6740408ac3cfa879cce73b703cf16674
kiril <kirillgrayson@gmail.com>
2022-02-02 15:08:02 +0200

    Moved to a different package

--- app/src/main/java/com/tetsoft/planshopping/adapter/grocerylist/GroceryListItemOnClickListener.kt
+++
@@ -1,9 +0,0 @@
-package com.tetsoft.planshopping.adapter.grocerylist
-
-import com.tetsoft.planshopping.db.entity.GroceryList
-
-class GroceryListItemOnClickListener (val clickListener: (groceryList: GroceryList) -> Unit) {
-    fun onClick(groceryList: GroceryList) {
-        clickListener(groceryList)
-    }
-}
