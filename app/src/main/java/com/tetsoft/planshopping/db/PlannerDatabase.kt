10921ebd6f87f884aeea923e5c64b76dfb0c3d2f
kiril <kirillgrayson@gmail.com>
2022-02-02 15:18:59 +0200

    Added DAOs

--- app/src/main/java/com/tetsoft/planshopping/db/PlannerDatabase.kt
+++ app/src/main/java/com/tetsoft/planshopping/db/PlannerDatabase.kt
@@ -5,16 +5,23 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
-import com.tetsoft.planshopping.db.entity.Product
-import com.tetsoft.planshopping.db.entity.GroceryList
-import com.tetsoft.planshopping.db.entity.SelectedProduct
+import com.tetsoft.planshopping.db.product.Product
+import com.tetsoft.planshopping.db.planned.PlannedList
+import com.tetsoft.planshopping.db.selected.SelectedProduct
+import com.tetsoft.planshopping.db.planned.PlannedListDao
+import com.tetsoft.planshopping.db.product.ProductDao
+import com.tetsoft.planshopping.db.selected.SelectedProductDao
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 
-@Database(entities = [Product::class, GroceryList::class, SelectedProduct::class], version = 2)
+@Database(entities = [Product::class, PlannedList::class, SelectedProduct::class], version = 3)
 abstract class PlannerDatabase : RoomDatabase() {
-    abstract fun plannerDao() : PlannerDao
+    abstract fun productDao() : ProductDao
+
+    abstract fun plannedListDao() : PlannedListDao
+
+    abstract fun selectedProductDao() : SelectedProductDao
 
     companion object {
         private var INSTANCE : PlannerDatabase? = null
@@ -27,6 +34,7 @@ abstract class PlannerDatabase : RoomDatabase() {
                     "PlannerDB"
                 )
                     .addCallback(PlannerDatabaseCallback(scope))
+                        // TODO: replace this with proper migration when release
                     .fallbackToDestructiveMigration()
                     .build()
             }
