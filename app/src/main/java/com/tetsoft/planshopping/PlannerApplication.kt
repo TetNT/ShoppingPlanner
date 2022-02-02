47b29f9752efdcc12efb46a713ec6b1796cd444c
kiril <kirillgrayson@gmail.com>
2022-02-02 15:17:54 +0200

    Added repositories declaration and coroutine scope

--- app/src/main/java/com/tetsoft/planshopping/PlannerApplication.kt
+++ app/src/main/java/com/tetsoft/planshopping/PlannerApplication.kt
@@ -1,20 +1,31 @@
 package com.tetsoft.planshopping
 
 import android.app.Application
-import com.tetsoft.planshopping.db.GroceryListRepository
+import com.tetsoft.planshopping.db.planned.PlannedListRepository
 import com.tetsoft.planshopping.db.PlannerDatabase
-import com.tetsoft.planshopping.db.ProductRepository
+import com.tetsoft.planshopping.db.product.ProductRepository
+import com.tetsoft.planshopping.db.selected.SelectedProductRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 
 class PlannerApplication : Application() {
-    val applicationScope = CoroutineScope(SupervisorJob())
+    private val applicationScope = CoroutineScope(SupervisorJob())
 
     val productRepository by lazy {
-        ProductRepository(PlannerDatabase.getDatabase(this, applicationScope).plannerDao())
+        ProductRepository(
+            PlannerDatabase.getDatabase(this, applicationScope).productDao()
+        )
     }
 
     val listsRepository by lazy {
-        GroceryListRepository(PlannerDatabase.getDatabase(this, applicationScope).plannerDao())
+        PlannedListRepository(
+            PlannerDatabase.getDatabase(this, applicationScope).plannedListDao()
+        )
+    }
+
+    val selectionRepository by lazy {
+        SelectedProductRepository(
+            PlannerDatabase.getDatabase(this, applicationScope).selectedProductDao()
+        )
     }
 }
