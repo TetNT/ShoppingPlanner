44889904c54bc6c9da4061908ab5d23213b19147
kiril <kirillgrayson@gmail.com>
2022-02-02 15:12:22 +0200

    Minor refactoring

--- app/src/main/java/com/tetsoft/planshopping/db/product/Product.kt
+++ app/src/main/java/com/tetsoft/planshopping/db/product/Product.kt
@@ -3,9 +3,9 @@ package com.tetsoft.planshopping.db.product
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "product_table")
+@Entity(tableName = "products_table")
 data class Product (
     @PrimaryKey(autoGenerate = true) val id: Int,
     val name: String,
     val price: Double
-        )
+)
