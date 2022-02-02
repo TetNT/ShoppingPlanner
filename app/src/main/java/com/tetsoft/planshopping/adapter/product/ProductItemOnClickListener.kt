0d665952cec03ebad7fee8686b9462eab4872dfa
kiril <kirillgrayson@gmail.com>
2022-02-02 15:20:14 +0200

    Package changed

--- app/src/main/java/com/tetsoft/planshopping/adapter/product/ProductItemOnClickListener.kt
+++ app/src/main/java/com/tetsoft/planshopping/adapter/product/ProductItemOnClickListener.kt
@@ -1,6 +1,6 @@
 package com.tetsoft.planshopping.adapter.product
 
-import com.tetsoft.planshopping.db.entity.Product
+import com.tetsoft.planshopping.db.product.Product
 
 class ProductItemOnClickListener(val clickListener: (product: Product) -> Unit) {
     fun onClick(product: Product) {
